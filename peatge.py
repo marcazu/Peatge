d15d9f61bc68483b1106b4d7885c32d41f36dc5c
marcazu <95marc@gmail.com>
2020-06-20 13:07:04 +0200

    Update peatge.py
    Falta acabar de complicar

--- peatge.py
+++ peatge.py
@@ -6,35 +6,45 @@ Created on Wed Jun 17 18:35:12 2020
 """
 
 import simpy
+import random
 
 NUM_PEATGES = 2 # numero de peatges actius
 NUM_RODES = [2,4,8]
 PREU_RODES = [6,7,10]
-TEMPS_PAGAMENT = 2 #temps que tarda en realitzar el pagametn un cotxe
+TEMPS_PAGAMENT = 15 #temps que la maquina tarda en realitzar un pagament en segons
 TEMPS_ARRIBADES = 0.5 # Temps entre arribades de cotxes
 
 
 class Peatge(object):
     """ Un petge té un nombre concret de barreres per on poden pasar els cotxes"""
+    maquinaPagament = 0
     def __init__(self,env, num_peatges, temps_pagament):
         self.env = env
-        self.machine = simpy.Resource(env,num_peatges)
+        self.maquinaPagament = simpy.Resource(env, num_peatges)
+        print ("numerp de peages")
+        print (num_peatges)
         self.temps_pagament = temps_pagament
     
     def pagar(self, numero):
-         yield self.env.timeout(TEMPS_PAGAMENT)
-         print("cotxe %s realitzant el pagament" % (numero))
+         print("El %s acaba la cua i comença a realitzar el pagametn a temps: %s." % (numero, env.now))
+         #temps que pot tardar una persona a realitzar el pagament
+         factorHuma = random.randrange(10, 30) 
+         tempsPrevi = env.now
+         yield self.env.timeout(TEMPS_PAGAMENT + factorHuma)
+         tempsPagament = env.now-tempsPrevi
+         print("El %s ha realitzat el pagament en %s segons" % (numero,tempsPagament))
         
 
 def cotxe(env, numero, peatg):
-    print("%s arriba al peatge en temps: %s. es disposa a fer cua" % (numero, env.now))
-    with peatg.machine.request() as request:   
-        yield request
+    tempsIni = env.now
+    print("El %s arriba a la cua del peatge a temps: %s." % (numero, env.now))
+    with peatg.maquinaPagament.request() as request:
+        result = yield request
          
-    print("%s entra al peatge i realitza el pagament a temps: %s." % (numero, env.now))
+
     yield env.process(peatg.pagar(numero))
-    
-    print("%s marxa del peatge a temps: %s." % (numero, env.now))
+    tempsTotal = env.now-tempsIni
+    print("%s marxa del peatge a temps: %s, ha tardat %s segons en completar tot el proces" % (numero, env.now, tempsTotal))
     
 def setUp(env, num_peatges, temps_pagament, temps_arribades):
     """crea un peatge amb un nonmbre concret de peatges, amb un temps de pagament
@@ -43,13 +53,17 @@ def setUp(env, num_peatges, temps_pagament, temps_arribades):
     peatge = Peatge(env, num_peatges, temps_pagament)
     
     # Create 4 initial cotxes
+    i = 0
+    """
     for i in range(4):
         env.process(cotxe(env, "Cotxe %d" % i, peatge))
-    # crea més cotxes mentres s'executa la simulació
+    # crea més cotxes mentres s'executa la simulació"""
     
     while True:
         try:
-            yield env.timeout(1)
+            "tarda entre 10 i 30 segons a crea un cotxe de forma aleatoria"
+            tempsEntreCotxes = random.randrange(5, 15)
+            yield env.timeout(tempsEntreCotxes)
             "yield env.timeout(random.randint(t_inter - 2, t_inter + 2))"
             i += 1
             env.process(cotxe(env, "Cotxe %d" % i, peatge))
@@ -59,6 +73,8 @@ def setUp(env, num_peatges, temps_pagament, temps_arribades):
     
 print("Peatge")
 env = simpy.Environment()
-p = env.process(setUp(env,NUM_PEATGES,TEMPS_PAGAMENT,TEMPS_ARRIBADES))
-env.run(50)
+p = env.process(setUp(env,NUM_PEATGES, TEMPS_PAGAMENT, TEMPS_ARRIBADES))
+env.run(100)
+
+
     
